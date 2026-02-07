17522d02a43164e7549ff5fd37add7e5a5fd9529
veronikas <syvaveronika@gmail.com>
2026-02-07 22:43:07 +0200

    chore(terraform): INIT-56 upgrade AWS provider and clean up config

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.31"
+    }
+  }
+}
+
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "eu-central-1"
 }
 
 variable "aws_profile" {
   description = "AWS CLI profile to use for authentication"
   type        = string
-  default     = "iac"
 }
 
 variable "bucket_name" {
